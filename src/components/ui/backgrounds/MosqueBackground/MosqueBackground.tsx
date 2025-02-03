2df0b1d60416b88ead5c0ba0dcb7a6ac326b9270
Jamoliddin Samadov <gamalsamadov@gmail.com>
2025-02-03 17:06:24 +0300

    Background image added

--- src/components/ui/backgrounds/MosqueBackground/MosqueBackground.tsx
+++ src/components/ui/backgrounds/MosqueBackground/MosqueBackground.tsx
@@ -1,10 +1,19 @@
+import Image from 'next/image'
 import { PropsWithChildren } from 'react'
+
 import styles from './MosqueBackground.module.scss'
 
 export const MosqueBackground = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <div className={styles['bg-mosque']}></div>
+      <div className={styles['bg-mosque']}>
+        <Image
+          src="/assets/vectors/mosque-3.svg"
+          width={1500}
+          height={1500}
+          alt="mosque"
+        />
+      </div>
 
       {children}
     </>
